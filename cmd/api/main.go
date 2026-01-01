219b1a48755dd3ec5665f1f91f2173783cc0bc80
Leo Claudio <11918571+zllec@users.noreply.github.com>
2026-01-01 11:55:15 +0800

    feat: add server and v1/healthcheck endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Declare a string containing the application version number. Later in the book we'll
+// generate this automatically at build time, but for now we'll just store the version
+// number as a hard-coded global constant.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our application.
+// For now, the only configuration settings will be the network port that we want the
+// server to listen on, and the name of the current operating environment for the
+// application (development, staging, production, etc.). We will read in these
+// configuration settings from command-line flags when the application starts.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
+// and middleware. At the moment this only contains a copy of the config struct and a
+// logger, but it will grow to include a lot more as our build progresses.
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
-	// This is a placeholder for the main function.
-	fmt.Println("Pitacca")
+	var cfg config
+
+	// Read the port and env cli flags into the config struct.
+	// Use the "4000" as the default port and "development" env
+	// if no corresponding flags are provided.
+	flag.IntVar(&cfg.port, "port", 4000, "API server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new structured logger which writes messages to the standard output
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Declare an instance of the application struct, containing the config and logger
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux and add a /v1/healthcheck
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Declare an HTTP server which:
+	// - listens on the port provided in the cfg struct
+	// - uses the servemux we created above as the handler
+	// - has some sensible timeout settings
+	// - writes any log messages to the structured logger at Error level
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// Start the HTTP server
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
