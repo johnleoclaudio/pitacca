a4394a4aee038861d7debdec4dfb3663a71a5bf8
Leo Claudio <11918571+zllec@users.noreply.github.com>
2026-01-06 21:28:14 +0800

    feat: add error handler helper

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -10,6 +10,17 @@ import (
 
 func (app *application) createUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, `create a new user`)
+
+	var input struct {
+		Email string `json:"email"`
+	}
+	err := app.readJSON(r, &input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) getUser(w http.ResponseWriter, r *http.Request) {
